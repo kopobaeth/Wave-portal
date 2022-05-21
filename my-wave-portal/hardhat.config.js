a1849ee3cfbb6609cb36b7b3415dd04678beba19
kopobaeth <refereumsuper@gmail.com>
2022-05-21 18:04:44 +0300

    Update hardhat.config.js

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
     solidity: "0.8.0",
     networks: {
         rinkeby: {
-            url: "https://eth-rinkeby.alchemyapi.io/v2/1kq4zFynJ9952xTCkgVaTOxGuwTLP51o",
-            accounts: ["61e8b7ca9336dbe704f342c4f237f5a3ccb4ab1a1c37e4d416f51148d6415f52"],
+            url: "your_alchemyapi",
+            accounts: ["your_private_key"],
         },
     },
 };
